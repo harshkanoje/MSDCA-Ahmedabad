d4b9f6de0c1248cc8d382957a6704be3f828973e
Pratyush Birole <birole.pratyush@gmail.com>
2024-01-22 13:51:54 +0530

    Fixed Banner Image

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,11 +3,11 @@ import React from 'react';
 function Header() {
   return (
     <div>
-      {/* Navbar Component starts here */}
       
-      <img src="./images/MSDCA (2) (1) (1).jpg" alt="MSDCA Logo" style={{maxWidth:"100%" , height: "auto" ,borderBottomLeftRadius: '50px', borderBottomRightRadius: '50px' }} />
+      
+      <img src="./images/backgroundbanner.png" alt="MSDCA Logo" style={{maxWidth:"100%" , height: "auto" ,borderBottomLeftRadius: '50px', borderBottomRightRadius: '50px' }} />
 
-      {/* Navbar Component ends here */}
+  
     </div>
   );
 }
