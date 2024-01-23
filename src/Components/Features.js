ac7934d08a6c61d48f6b6ef82805c7a993160110
dev_hash <129186823+harshkanoje@users.noreply.github.com>
2024-01-24 01:03:50 +0530

    Update Features.js
    Responsive

--- src/Components/Features.js
+++ src/Components/Features.js
@@ -11,9 +11,13 @@ function Features() {
   };
   const contentStyle = {
     flex: 1,
-    fontSize: "15px",
+    // fontSize: "15px",
     fontWeight: "bold",
-    color: 'rgba(0, 0, 0, 0.7)'
+    color: 'rgba(0, 0, 0, 0.7)',
+    // overflow: 'hidden',      // Added to handle overflow
+    // whiteSpace: 'nowrap' ,
+    fontSize: '1.2em', // Using relative units for font size
+
   };
 
   return (
@@ -53,51 +57,92 @@ function Features() {
           border-style: solid;
           border-width: 1px;
         }
+      
+        @media (max-width: 768px) {
+          .animated-card {
+            width: 100%;
+            height: 80px; /* Reduce the height for smaller screens */
+            margin: 0 0 20px 0;
+
+          }
+
+          .animated-card:hover ~ .animated-card {
+            position: relative;
+            left: 0;
+          }
+        }
+
+        @media (min-width: 769px) and (max-width: 1024px) {
+          .animated-card {
+            width: calc(50% - 20px);
+            height: 250px; /* Adjust the height for medium-sized screens */
+            font-size : 2rem
+          }
+
+          .animated-card:hover ~ .animated-card {
+            position: relative;
+            left: 20px;
+          }
+        }
+
+        @media (min-width: 1025px) and (max-width: 1440px) {
+          .animated-card {
+            width: calc(33.33% - 20px);
+            height: 300px; /* Adjust the height for larger screens */
+          }
+
+          .animated-card:hover ~ .animated-card {
+            position: relative;
+            left: 20px;
+          }
+        }
+
+
+
         `}
       </style>
       <br />
-      
-      <div style={{ backgroundColor: "#EFF2FF", height: "400px", display: "flex", justifyContent: "center", marginRight:"20px",borderRadius: "0 0 50px 0", alignItems: "center" }}>
-      
-        <div className="card animated-card" style={{ width: '18rem', borderRadius: "20px", borderColor: "white" }}>
-          <div className="card-body">
-            <h5 className="card-title" style={{ fontFamily: 'PT sans', color: '#65A0FB', fontSize: "80px", marginTop: "50px" }}>49</h5>
+
+      <div style={{ backgroundColor: "#EFF2FF", height: "400px", display: "flex", justifyContent: "center", marginRight: "20px", borderRadius: "0 0 50px 0", alignItems: "center", flexWrap: 'wrap' }}>
+
+        <div className="card animated-card" style={{ width: '15rem', borderRadius: "20px", borderColor: "white" }}>
+          <div className="card-body overflow: 'hidden', whiteSpace: 'nowrap' ">
+            <h5 className="card-title" style={{ fontFamily: 'PT sans', color: '#65A0FB', fontSize: "60px", marginTop: "50px" }}>49</h5>
             <p className="card-text" style={contentStyle}>
               Number of Academies.
             </p>
           </div>
         </div>
 
-        <div className="card animated-card" style={{ width: '18rem', borderRadius: "20px", borderColor: "white" }}>
+        <div className="card animated-card" style={{ width: '15rem', borderRadius: "20px", borderColor: "white", flexWrap: 'wrap' }}>
           <div className="card-body">
-            <h5 className="card-title" style={{ fontFamily: 'PT sans', color: '#65A0FB', fontSize: "80px", marginTop: "50px" }}>10K +</h5>
+            <h5 className="card-title" style={{ fontFamily: 'PT sans', color: '#65A0FB', fontSize: "60px", marginTop: "50px" }}>10K +</h5>
             <p className="card-text" style={contentStyle}>
               Academy Students.
             </p>
           </div>
         </div>
 
-        <div className="card animated-card" style={{ width: '18rem', borderRadius: "20px", borderColor: "white" }}>
+        <div className="card animated-card" style={{ width: '15rem', borderRadius: "20px", borderColor: "white" }}>
           <div className="card-body">
-            <h5 className="card-title" style={{ fontFamily: 'PT sans', color: '#65A0FB', fontSize: "80px", marginTop: "50px" }}>10K +</h5>
+            <h5 className="card-title" style={{ fontFamily: 'PT sans', color: '#65A0FB', fontSize: "60px", marginTop: "50px" }}>10K +</h5>
             <p className="card-text" style={contentStyle}>
               Online Students
             </p>
           </div>
         </div>
 
-        <div className="card animated-card" style={{ width: '18rem', borderRadius: "20px", borderColor: "white" }}>
+        <div className="card animated-card" style={{ width: '15rem', borderRadius: "20px", borderColor: "white" }}>
           <div className="card-body">
-            <h5 className="card-title" style={{ fontFamily: 'PT sans', color: '#65A0FB', fontSize: "80px", marginTop: "50px" }}>650</h5>
+            <h5 className="card-title" style={{ fontFamily: 'PT sans', color: '#65A0FB', fontSize: "60px", marginTop: "50px" }}>650</h5>
             <p className="card-text" style={contentStyle}>
               Online Certifications.
             </p>
           </div>
         </div>
-        <br />
-        <br />
-        <br />
-        <br />
+
+
+
       </div>
     </div>
   );
